d9b752979ded5123941ef0e00636985182e813f4
rwokmrwg <goida991gol@gmail.com>
2025-06-20 18:02:43 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scaner = new Scanner(System.in);
+        Random random = new Random();
+        int secretNumber = random.nextInt(100)+ 1;
+        int attempts = 0;
+        int guess;
+        System.out.println(" Привет , я загадал число от 1 до 100 попробуй отгадать!");
+        while(true){
+            System.out.println("Твоя догадка:");
+            guess = scaner.nextInt();
+            attempts++;
+            if(guess < secretNumber){
+                System.out.println("Слишком маленькое число");
+            } else if(guess > secretNumber){
+                System.out.println("Слишком большое число");
+            }else {
+                System.out.println("Ура ты победил");
+                break;
+            }
+        }
+        scaner.close();
+
+    }
+}
